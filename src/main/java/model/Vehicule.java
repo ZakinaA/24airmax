1472abda77e0b0cc3b2a5c1ed36823ab27d324c2
User <ts1sio@SIO301-15.sio.bts>
2024-03-28 11:54:39 +0100

    Rajout des propriété dateOrigine et dateRevision avec leur get/set dans la classe Vehicule

--- src/main/java/model/Vehicule.java
+++ src/main/java/model/Vehicule.java
@@ -1,16 +1,22 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Vehicule {
     private int id;
     private String immat;
+    private LocalDate dateOrigine;
+    private LocalDate dateRevision;
     
     public Vehicule(){
         
     }
     
-    public Vehicule(int id, String immat){
+    public Vehicule(int id, String immat, LocalDate dateOrigine, LocalDate dateRevision){
         this.id=id;
         this.immat=immat;
+        this.dateOrigine=dateOrigine;
+        this.dateRevision=dateRevision;
     }
 
     public int getId() {
@@ -28,5 +34,22 @@ public class Vehicule {
     public void setImmat(String immat) {
         this.immat = immat;
     }
+
+    public LocalDate getDateOrigine() {
+        return dateOrigine;
+    }
+
+    public void setDateOrigine(LocalDate dateOrigine) {
+        this.dateOrigine = dateOrigine;
+    }
+
+    public LocalDate getDateRevision() {
+        return dateRevision;
+    }
+
+    public void setDateRevision(LocalDate dateRevision) {
+        this.dateRevision = dateRevision;
+    }
+    
     
 }
